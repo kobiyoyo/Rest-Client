5b787bce1b5af04a7039e4c6359f332c51f5a8c4
chubi adama <adamachubi@gmail.com>
2019-09-18 12:14:31 +0100

    corrected errors

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 def http(url)
   good_url = url.split(' ')
   good_url.join('+')
-  search_url = RestClient.get('https://www.bing.com/search',params:{q:'#{good_url}'})
+  search_url = RestClient.get('https://www.bing.com/search', params: { q: good_url.to_S })
   puts search_url.code
- 
 end
 search = gets.chomp
 http(search)
